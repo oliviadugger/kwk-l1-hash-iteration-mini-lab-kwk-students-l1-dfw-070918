ede98abffa2eab82062a0dff77640b16434edcd2
oliviadugger <oliviacdugger@gmail.com>
2018-07-11 19:40:02 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -18,10 +18,6 @@ def iterate_through_hash
   end 
 end
 
-# Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
-  
-  
   
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
