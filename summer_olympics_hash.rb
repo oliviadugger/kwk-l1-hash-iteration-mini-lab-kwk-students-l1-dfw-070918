7949e07114903db4cefd4ff979db58db6bb5a9e6
oliviadugger <oliviacdugger@gmail.com>
2018-07-11 19:52:05 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -21,7 +21,7 @@ end
   
 def iterate_through_keys
 upcased_cities = ["Sydney", "Athens", "Beijing",  "London"]
-  upcased_cities.upcase
+  upcased_cities.each
 end
 
 # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
